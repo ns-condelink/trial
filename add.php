fac8bfa8d04084e9751021476641c4837e61a124
Narendra Singh <ns@codelinkinfotech.com>
2022-06-14 16:54:06 +0530

    Initial commit

---
+++ add.php
@@ -0,0 +1,12 @@
+<?php
+
+
+//function to add two number
+function add($x, $y){
+    return $x + $y;
+}
+
+echo add(4, 8);
+
+
+?>
---
+++ mul.php
@@ -0,0 +1,12 @@
+<?php
+
+
+//function to multiply two number
+function mul($x, $y){
+    return $x * $y;
+}
+
+echo mul(4, 8);
+
+
+?>
